f0b56e77a7a58399c37d81071097798555b5e593
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-01-04 12:19:29 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5{
+    public static void main(String[] args) {
+        String username = "abhi";
+        String pass = "abhi141";
+        while ((username == "abhi")&&(pass == "abhi141")) {
+            System.out.println("hello user");
+            break;
+
+        }
+    }
+}
