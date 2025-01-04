6d07bfd8a239def504184c48f3b9f0c1b58e998c
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-01-04 11:58:47 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,19 @@
+class Demo3{
+    public static void main (String[] args)
+    {
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        if (num1>num2 & num1>num3){
+            System.out.println("num1 is greater then num2 and num3");
+        }
+        else if(num2>num1 & num2>num3) {
+            System.out.println("num2 is grater");
+            
+        } else {
+            
+        
+            System.out.println("num3 is greater");
+        }
+    }
+}
