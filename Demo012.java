3035a20f3d525e79c7cd4f57f0eba2826d937fc5
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-01-07 12:49:05 +0530

    Add files via upload

---
+++ Demo012.java
@@ -0,0 +1,13 @@
+package ps001.corejava;
+
+public class Demo012 {
+    public static void main(String args[]){
+        double radius = 7.0;
+        double area = Math.PI*Math.pow(radius,2);
+        System.out.println("Area of the Circle "+ radius + "is"+ area);
+
+    }
+}
+
+
+
---
+++ Demo013.java
@@ -0,0 +1,10 @@
+package ps001.corejava;
+
+public class Demo013 {
+    public static void main(String args[]){
+        int base = 20;
+        int heigt = 20;
+        double area = 0.5*base*heigt;
+        System.out.println(area);
+    }
+}
