bf686772cb3180e3ebce7e1f477f3a3c9d3fcad2
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-01-07 13:20:45 +0530

    Add files via upload

---
+++ Demo17.java
@@ -0,0 +1,10 @@
+package ps001.corejava;
+
+public class Demo17 {
+    public static void main(String[] args) {
+        int a =25;
+        int b = 35;
+        int   sum = a+b;
+        System.out.println(sum);
+    }
+}
