86263b648bef4212c7afb30b8764ffa3d2115aa2
abhi141Y <168571932+abhi141Y@users.noreply.github.com>
2025-01-08 12:45:13 +0530

    Add files via upload

---
+++ NextMultipleOf.java
@@ -0,0 +1,8 @@
+package codejava.PS002;
+class NextMultipleOf100{
+    public static void main(String[] args) {
+        int number= 305;
+        int NextMultiple = (number/100+1)*100;
+        System.out.println(NextMultiple);
+    }
+}
